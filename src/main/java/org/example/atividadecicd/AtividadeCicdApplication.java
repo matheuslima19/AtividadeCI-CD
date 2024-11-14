37eaf07257afd41671d50afdee9969ed27a3a540
matheuslima19 <matheus.slima@sptech.school>
2024-11-13 21:36:08 -0300

    Atividade edu

---
+++ src/main/java/org/example/atividadecicd/AtividadeCicdApplication.java
@@ -0,0 +1,13 @@
+package org.example.atividadecicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtividadeCicdApplication.class, args);
+    }
+
+}
